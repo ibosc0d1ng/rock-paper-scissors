b7df404aa6c3340d38ac6f4111b6e14406ba937b
1b0 <iboiscoding@gmail.com>
2022-10-21 02:19:11 +0200

    started and completed rps project

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function random(number) {
-    return Math.floor(Math.random() * number + 1);
-    }
-    
-function capitalize(string) {
-    return (
-      string.toLowerCase().charAt(0).toUpperCase() + string.toLowerCase().slice(1)
+const images = Array.from(document.querySelectorAll('.card-image'));
+const scorePlayer = document.querySelector('.player-score')
+const scoreComputer = document.querySelector('.computer-score')
+const selectionPlayer = document.querySelector('.player-selection')
+const selectionComputer = document.querySelector('.computer-selection')
+const message = document.querySelector('.message');
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+    images.forEach((image) => 
+        image.addEventListener('click', () => {
+            if (playerScore >= 5 || computerScore >= 5) {
+                return;
+              }
+            game(image.dataset.image);
+        })
     );
-    }
+
 
 function getComputerChoice() {
     let computerNumber = random(3)
@@ -26,12 +38,6 @@ function getComputerChoice() {
         break;
     }
     return computerGuess;
-}
-
-function getPlayerSelection(){
-    let playerSelection = prompt('Rock, Paper or Scissors ?');
-    playerSelection = capitalize(playerSelection);
-    return playerSelection;
 }
 
 
@@ -69,39 +75,49 @@ function playRound(playerSelection, computerSelection){
 return log;
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
+function createParaWithText(text) {
+    const p = document.createElement('p');
+    p.textContent = text;
 
-    for (let i = 0; i < 5; i++) {
+    return p;
+}
+
+
+function game(playerSelect){
+
     let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerSelection();
+    let playerSelection = capitalize(playerSelect);
     let roundResult = playRound(playerSelection, computerSelection);
-    console.log(
-    `Player: ${playerSelection} vs Computer: ${computerSelection}` )
-    
-    console.log(roundResult);
 
     if (roundResult.search('You Won') > -1){
         playerScore++;
     } else if (roundResult.search('You Lost') > -1){
         computerScore++;
     }
+
+
+
+scorePlayer.textContent = playerScore;
+scoreComputer.textContent = computerScore;
+
+selectionPlayer.appendChild(createParaWithText(playerSelection));
+selectionComputer.appendChild(createParaWithText(computerSelection));
+
+if (playerScore >= 5 && computerScore < 5) {
+    message.textContent = 'Game Over. You Win!';
+  } else if (playerScore < 5 && computerScore >= 5) {
+    message.textContent = 'Game Over. You Lose!';
+  }
+
 }
 
-    console.log('Final Results')
-    console.log(
-        `Player Score: ${playerScore} & Computer Score: ${computerScore}`
-    );
 
-    if (playerScore < computerScore){
-        console.log('You Lost!')
-    } else if (playerScore > computerScore){
-        console.log("You Won!")
-    } else {
-        console.log("It's a Tie!")
+function random(number) {
+    return Math.floor(Math.random() * number + 1);
     }
-
-}
-
-console.log(game());
+    
+function capitalize(string) {
+    return (
+      string.toLowerCase().charAt(0).toUpperCase() + string.toLowerCase().slice(1)
+    );
+    }
